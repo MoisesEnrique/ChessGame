34d94a86925fc9b804513c77f8f7120aa5f3465c
WFkSky <moises.mac@gmail.com>
2020-09-27 15:16:38 -0500

    First commit

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include "mainwindow.h"
+
+#include <QApplication>
+
+#include <iostream>
+
+
+int main(int argc, char *argv[])
+{
+    std::cout<<"HELLO"<<std::endl;
+
+    /*
+    QApplication a(argc, argv);
+    MainWindow w;
+    w.show();
+    return a.exec();
+    */
+}
